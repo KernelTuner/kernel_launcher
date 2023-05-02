c9653770dc820b0433a4ff28ed4b1e005931b269
stijn <s.heldens@esciencecenter.nl>
2023-05-02 20:28:46 +0200

    Add `pragma.h` to `kernel_launcher.h`

--- include/kernel_launcher.h
+++ include/kernel_launcher.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "kernel_launcher/arg.h"
+#include "kernel_launcher/builder.h"
 #include "kernel_launcher/compiler.h"
 #include "kernel_launcher/config.h"
 #include "kernel_launcher/cuda.h"
+#include "kernel_launcher/export.h"
 #include "kernel_launcher/expr.h"
 #include "kernel_launcher/fs.h"
 #include "kernel_launcher/kernel.h"
+#include "kernel_launcher/pragma.h"
 #include "kernel_launcher/registry.h"
 #include "kernel_launcher/utils.h"
 #include "kernel_launcher/value.h"
